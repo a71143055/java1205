3fea564f1edef934f7442d8bb3fec798eb3f7d4e
a71143055 <a71143055@gmail.com>
2024-12-05 10:42:33 +0900

    java1205

---
+++ src/GUI/controller/MainFrame.java
@@ -0,0 +1,29 @@
+package GUI.controller;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+
+    public static final int FRAME_WIDTH = 600;
+    public static final int FRAME_HEIGHT = 500;
+
+    public static int FRAME_X = 300;
+    public static int FRAME_Y = 200;
+
+    public MainFrame() {
+        super("검색프로그램");
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension dimension = toolkit.getScreenSize();
+        double sw = dimension.getWidth();
+        double sh = dimension.getHeight();
+
+        setLocation(FRAME_X, FRAME_Y);
+        setSize(FRAME_WIDTH,FRAME_HEIGHT);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MainFrame();
+    }
+}
